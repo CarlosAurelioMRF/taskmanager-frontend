a5a847c0f5aaa4badb6d0095b76f4c13da6f264b
Carlos Aurelio Meireles Ribeiro Filho <carlosspok@gmail.com>
2017-08-31 21:45:40 -0300

    Binding from logic to view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LearningBindingsComponent } from './learning-bindings/learning-bindings.component';
@@ -10,7 +11,8 @@ import { LearningBindingsComponent } from './learning-bindings/learning-bindings
     LearningBindingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
