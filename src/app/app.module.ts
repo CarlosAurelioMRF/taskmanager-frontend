21d2190502abbd01941c9c354868e61b17f791e0
Carlos Aurelio Meireles Ribeiro Filho <carlosspok@gmail.com>
2017-09-02 16:52:00 -0300

    adding http to TaskService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -23,6 +24,7 @@ import { AppRoutingComponent } from "./app-routing.module";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingComponent
   ],
   providers: [ TaskService ],
