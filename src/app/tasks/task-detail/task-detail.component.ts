ca19db9f8d12f7d2632b8d3db252bb02c70597d3
Carlos Aurelio Meireles Ribeiro Filho <carlosspok@gmail.com>
2017-09-02 20:32:23 -0300

    updating tasks

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -35,4 +35,14 @@ export class TaskDetailComponent implements OnInit {
         this.location.back();
     }
 
+    public updateTask() {
+        if(!this.task.title) {
+            alert("A tarefa deve ter um titulo");
+        } else {
+            this.taskService.updateTask(this.task)
+                .subscribe(() => alert("Tarefa atualizada com sucesso."),
+                           () => alert("Ocorreu um erro no servidor, tente mais tarde"));
+        }
+    }
+
 }
